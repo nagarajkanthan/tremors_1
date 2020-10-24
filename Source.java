4c4d7f75cc959787692a553e870fb024e574c4f2
nagarajKanthan <nagaraj.kanthan@gmail.com>
2020-10-24 11:15:13 +0530

    test commit

---
+++ Source.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+class Rectangle{
+    public void printNameOfClass(){
+        System.out.println("Rectangle");
+    }
+}
+class Square extends Rectangle{
+    //@Override
+    public void printNameofClass(){
+        System.out.println("Square");
+    }
+}
+
+public class Source{
+    public static void main(String[] args){
+        Square s = new Square();
+        s.printNameOfClass();
+    }
+}
